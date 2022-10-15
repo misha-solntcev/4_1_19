2f6e832d44c7edc4b35f48293bb83e9f6ca68098
misha-solntcev <misha.solntcev@gmail.com>
2022-10-15 17:56:58 +0800

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Вариант 19
+Дана целочисленная матрица A(NM). Найдите номер первой из ее строк, которые
+начинаются с К положительных чисел подряд.*/
+
+namespace _4_1_19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
