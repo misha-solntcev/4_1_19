4eb5d02201080d240ead970b789cf85d163aea90
misha-solntcev <misha.solntcev@gmail.com>
2022-10-15 18:12:46 +0800

    Финиш

--- Program.cs
+++ Program.cs
@@ -14,6 +14,36 @@ namespace _4_1_19
     {
         static void Main(string[] args)
         {
+            int[,] arr = new int[,]
+            {   { 8, 0, -6, -4, 8 },
+                { -4, 1, 6, 7, 8 },
+                { 7, 8, 9, 1, -1 },
+                { 12, 0, 3, 4, 5 }  };
+            int n = arr.GetLength(0);
+            int m = arr.GetLength(1);
+
+            int k = 4;
+            int index = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int count = 1;
+                for (int j = 0; j < m-1; j++)
+                {
+                    if (arr[i, 0] > 0)
+                    {
+                        if ((arr[i, j] > 0) && (arr[i, j + 1] > 0))
+                            count++;
+                    }                    
+                }
+                if (count >= k)
+                {
+                    index = i;
+                    goto Exit;
+                }
+            }
+        Exit:
+            Console.WriteLine(index);
+            Console.ReadKey();
         }
     }
 }
